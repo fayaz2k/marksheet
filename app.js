a7e203fe40863bc1029816bf7e9567d8574d8948
unknown <fayazali2k@gmail.com>
2017-03-06 10:16:03 -0800

    marksheet complt

---
+++ app.js
@@ -0,0 +1,21 @@
+var a;
+
+a = +prompt("Grade Calculater","Enter Your Marks");
+if(a>=90){
+    alert("Dude You Got A-1 Grade");
+}
+else if(a>=80){
+    alert("Dude you Got A Grade");
+}
+else if(a>=70){
+    alert("Dude You Got B Grade");
+}
+else if(a>=60){
+    alert("Dude You Got C Grade");
+}
+else if(a>=50){
+    alert("Dude Your Got D Grade");
+}
+else if(a<=49){
+    alert("FAIL ! Are Your Serious, Where were you when lecture's was going on ' _ ' ");
+}
